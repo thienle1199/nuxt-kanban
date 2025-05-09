51caea992cc755107c9a6ee9234c6f02b9a9a510
Le Luong <thienle.luong@gmail.com>
2025-05-09 10:10:41 +0700

    integrate supabase

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
+<script setup lang="ts">
+import { cn } from './lib/utils';
+
+const supbase = useSupabaseClient()
+const { data } = useAsyncData('boards', async () => {
+  const { data } = await supbase.from('boards').select('*')
+  return data
+})
+
+const isSidebarVisible = true;
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <aside :class="cn(
+      'fixed top-0 left-0 hidden h-full z-40 w-[300px] border-r overflow-hidden border-divider py-6 pr-6 mobile:flex flex-col bg-sidebar',
+      {
+        'translate-x-0': isSidebarVisible,
+        '-translate-x-full': !isSidebarVisible,
+        'transition-transform duration-300 ease-in-out': true,
+      }
+    )">
+    <h2 className="text-xs font-bold tracking-[.2em] px-6 mb-4">
+      ALL BOARDS ({{ data?.length }})
+    </h2>
+    <div v-for="board in data" :key="board.id">
+      <BoardItem :name="board.name" :id="board.id" />
+    </div>
+  </aside>
+  <NuxtPage />
+</NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts']
+  modules: ['@nuxt/fonts', 'shadcn-nuxt', '@nuxtjs/supabase'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  css: ['~/assets/css/main.css'],
+  pages: true,
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
